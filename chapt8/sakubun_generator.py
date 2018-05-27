e7ae719be89709c543f58c85301b4b7a49979c8c
NHRD <naohisa.harada@gmail.com>
2018-05-27 21:36:42 +0900

    a

---
+++ chapt8/sakubun_generator.py
@@ -0,0 +1,7 @@
+import re
+
+def sakubun_gen(adj, noum, verb, adv):
+
+sakubun_file = open(".\\Automate-the-boring-stuff-with-python\\chapt8\\sakubun.txt")
+sakubun = sakubun_file.read()
+print(sakubun)
