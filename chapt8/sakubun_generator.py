b60cb83e956d40275131a740456d32610a55b456
NHRD <naohisa.harada@gmail.com>
2018-05-28 21:02:25 +0900

    a

--- chapt8/sakubun_generator.py
+++ chapt8/sakubun_generator.py
@@ -1,7 +1,35 @@
 import re
 
-def sakubun_gen(adj, noum, verb, adv):
+def sakubun_gen(adj, noun, verb, adv):
+    sakubun_file = open(".\\Automate-the-boring-stuff-with-python\\chapt8\\sakubun.txt", "r")
+    sakubun = sakubun_file.read()
+    sakubun_list = sakubun.split(" ")
+    sakubun_out = []
+    for i in sakubun_list:
+        if re.compile(r"^ADJECTIVE").search(i) != None:
+            i = re.compile(r"^ADJECTIVE").sub(adj, i)
+            sakubun_out.append(i)
+        elif re.compile(r"^NOUN").search(i) != None:
+            i = re.compile(r"^NOUN").sub(noun, i)
+            sakubun_out.append(i)
+        elif re.compile(r"^VERB").search(i) != None:
+            i = re.compile(r"^VERB").sub(verb, i)
+            sakubun_out.append(i)
+        elif re.compile(r"^ADVERB").search(i) != None:
+            i = re.compile(r"^ADVERB").sub(adv, i) 
+            sakubun_out.append(i)
+        else:
+            sakubun_out.append(i)
+    result = " ".join(sakubun_out)
+    sakubun_file = open(".\\Automate-the-boring-stuff-with-python\\chapt8\\sakubun_new.txt", "w")
+    sakubun_file.write("{}".format(result))
+    return result
 
-sakubun_file = open(".\\Automate-the-boring-stuff-with-python\\chapt8\\sakubun.txt")
-sakubun = sakubun_file.read()
-print(sakubun)
+adj = str(input("Enter an adjective:\n"))
+noun = str(input("Enter a noun:\n"))
+verb = str(input("Enter a verb:\n"))
+adv = str(input("Enter an adverb:\n"))
+
+result = sakubun_gen(adj, noun, verb, adv)
+
+print(result)
