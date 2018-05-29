ed51bce2c5024ac6d5850f4443273b870f4e9bbf
NHRD <naohisa.harada@gmail.com>
2018-05-29 21:22:45 +0900

    a

--- chapt8/regex_sort.py
+++ chapt8/regex_sort.py
@@ -1,3 +1,22 @@
 import re, os
 
+def regex_sercher(regex):
+    result_list = []
+    file_list = os.listdir(".\\Automate-the-boring-stuff-with-python\\chapt8\\regex_sort_files")
+    #print(file_list)
+    
+    for i in file_list:
+        path = ".\\Automate-the-boring-stuff-with-python\\chapt8\\regex_sort_files\\" + i
+        text_file = open(path, "r")
+        text = text_file.read()
+        text_list = text.split("\n")
+        for j in text_list:
+            if re.compile((regex)).search(j) != None:
+                result_list.append(re.compile((regex)).search(j).group())
+        text = text_file.close()
 
+    result = "\n".join(result_list)
+    return result
+
+test = regex_sercher(r"Charlie")
+print(test)
