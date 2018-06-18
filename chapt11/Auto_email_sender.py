443910975b073ca60a4d7a683186c40363f6e868
NHRD <naohisa.harada@gmail.com>
2018-06-18 21:39:33 +0900

    a

--- chapt11/Auto_email_sender.py
+++ chapt11/Auto_email_sender.py
@@ -6,12 +6,15 @@ browser = webdriver.Firefox()
 browser.get("https://login.yahoo.co.jp")
 email_elm = browser.find_element_by_id("username")
 
-email_elm.send_keys("email_address")
+#email_elm.send_keys("email_address")
+email_elm.send_keys("hrd_stad@yahoo.co.jp")
 email_elm.submit()
-password_elm = browser.find_element_by_id("passwd")
 
-password_elm.send_keys("mypassword")
+password_elm = browser.find_element_by_id("passwd")
+#password_elm.send_keys("mypassword")
+password_elm.send_keys("k2furs93")
 password_elm.submit()
+
 new_email = browser.find_element_by_id("main-btn-new")
 new_email.submit()
 
