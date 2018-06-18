7f98dba76dce950c93d1375aa31ef4e2f7c0285b
NHRD <naohisa.harada@gmail.com>
2018-06-18 21:26:23 +0900

    a

--- chapt11/Auto_email_sender.py
+++ chapt11/Auto_email_sender.py
@@ -1,14 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 browser = webdriver.Firefox()
-browser.get("https://login.yahoo.co.jp/config/login?.src=ym&.done=https%3A%2F%2Fmail.yahoo.co.jp%2F")
+browser.get("https://login.yahoo.co.jp")
 email_elm = browser.find_element_by_id("username")
-email_address = input("Input sender email address:")
-email_elm.send_keys(email_address)
+
+email_elm.send_keys("email_address")
 email_elm.submit()
 password_elm = browser.find_element_by_id("passwd")
-mypassword = input("Input password:")
-password_elm.send_keys(mypassword)
+
+password_elm.send_keys("mypassword")
 password_elm.submit()
 new_email = browser.find_element_by_id("main-btn-new")
 new_email.submit()
+
+receiver_elm = browser.find_element_by_id("to")
+receiver_elm.send_keys("reciever email address")
+
+subject_elm = browser.find_element_by_id("subject")
+receiver_elm.send_keys("test")
+
+content_elm = browser.find_element_by_id("rtetext")
+content_elm.send_keys("test")
+
+actions = ActionChains(browser)
+actions.key_down(Keys.CONTROL)
+actions.key_down(Keys.ENTER)
