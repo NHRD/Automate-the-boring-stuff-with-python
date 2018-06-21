25e9597fd74622972aa620cd582be1e5e72fd4bc
NHRD <naohisa.harada@gmail.com>
2018-06-21 21:22:50 +0900

    a

--- chapt11/link_downloader.py
+++ chapt11/link_downloader.py
@@ -5,13 +5,17 @@ def link_checker(hp_url):
     links = []
     res = requests.get(hp_url)
     res.raise_for_status()
-    res_bs = bs4.BeautifulSoup(res)
+    res_bs = bs4.BeautifulSoup(res.text)
     hp_list = res_bs.select("#list h2 a")
-    for i in hp_list:
-        links.append(i.get("href"))
+    for i in range(len(hp_list)):
+        links.append(hp_list[i].get("href"))
     return links
 
-def hp_opener(link):
+def hp_opener(links):
     browser = webdriver.Firefox()
+    for link in links:
+        browser.open(link)
 
-hp_url = input("Input target URL:")
+def main():
+    hp_url = input("Input target URL:")
+    hp_opener(link_checker(hp_url))
